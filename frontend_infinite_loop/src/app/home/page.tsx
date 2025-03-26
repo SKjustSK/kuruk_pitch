96ba3da99c25a5b795fbeb8b89d78b48d6babb71
Ayush Kumar <sk5020188@gmail.com>
2025-03-27 01:13:40 +0530

    improved the UI

--- frontend_infinite_loop/src/app/home/page.tsx
+++ frontend_infinite_loop/src/app/home/page.tsx
@@ -1,7 +1,3 @@
-
-
-
-
 "use client";
 
 import Image from "next/image";
@@ -20,32 +16,38 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-gray-900 to-white-900 text-white">
      
-      <nav className="bg-blue-500 bg-opacity-10 left-0 flex justify-between items-center px-10 py-5 fixed w-full top-0 z-50 shadow-lg">
-        <h1 className="text-5xl font-bold">EagleEyes</h1>
+      <nav className="bg-gray-500 bg-opacity-10 left-0 flex justify-between items-center px-10 py-5 fixed w-full top-0 z-50 shadow-lg">
+        <h1 className="text-3xl font-bold">EagleEyes</h1>
         <div className="flex gap-6">
           <button
             onClick={() => scrollToSection("home")}
-            className="hover:text-black text-2xl mr-10 ml-0"
+            className="hover:text-black text-1xl mr-10 ml-0"
           >
             Home
           </button>
           <button
             onClick={() => scrollToSection("crowd")}
-            className="hover:text-black text-2xl mr-10"
+            className="hover:text-black text-1xl mr-10"
           >
             Crowd Management
           </button>
           <button
             onClick={() => scrollToSection("target")}
-            className="hover:text-black text-2xl mr-10"
+            className="hover:text-black text-1xl mr-10"
           >
             Target Detection
           </button>
           <button
             onClick={() => scrollToSection("crime")}
-            className="hover:text-black text-2xl mr-10"
+            className="hover:text-black text-1xl mr-10"
           >
             Crime Prevention
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl border-blue-50">
+            Register
+          </button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg border-blue-50">
+            Sign In
           </button>
         </div>
       </nav>
@@ -58,8 +60,8 @@ export default function Home() {
         <div className="text-left text-gray-900">
           <h2 className="text-4xl font-bold mb-4">Advanced CCTV Monitoring</h2>
           <p className="text-lg mb-6">
-            Real-time surveillance with AI-based anomaly detection and <br></br>
-            high-definition monitoring for total security.
+            Real-time surveillance with AI-based anomaly detection  <br></br>
+            and high-definition monitoring for <b>crowd management, <br /> crime prevention, </b> and <b>work monitoring</b> 
           </p>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
             Learn More
@@ -80,72 +82,77 @@ export default function Home() {
       {/* Crowd Management section  */}
       <section
         id="crowd"
-        className="flex items-center justify-center h-screen px-10 bg-blue-100"
+        className="flex items-center justify-center h-screen px-10 bg-gray-100"
       >
-        <div className="text-left text-gray-900">
-          <h2 className="text-4xl font-bold mb-4">Crowd Management</h2>
-          <p className="text-lg mb-6">
-            AI-powered analytics to monitor and manage large crowds efficiently.
-          </p>
-          <Image
+        <Image
             src="/crowd3.jpg"
             alt="Crowd Management"
             width={500}
             height={400}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg ml-30"
           />
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
+        <div className="text-left text-gray-900 ml-20">
+          <h2 className="text-4xl font-bold mb-4">Crowd Management</h2>
+          <p className="text-lg mb-6">
+            AI-powered analytics to monitor the crowd at important places <br /> and to prevent any condition of stampede.
+          </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg mr-50 mt-5">
             Manage Crowd
           </button>
+        </div>
+        
+        
+          
       </section>
 
       {/* Target Detection Section */}
       <section
         id="target"
-        className="flex items-center justify-center h-screen px-10 bg-green-100"
+        className="flex items-center justify-center h-screen px-10 bg-gray-100"
       >
-        <div className="text-left text-gray-900">
+        <div className="text-left text-gray-900 ml-50 mr-10">
           <h2 className="text-4xl font-bold mb-4">Target Detection</h2>
           <p className="text-lg mb-6">
-            Advanced object recognition and tracking to identify potential
-            threats in real-time.
+            Advanced person recognition and tracking to identify potential <br />
+            criminal or missing person in large and small cities.
           </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
+            Detect Target
+          </button>
+        </div>
+        
           <Image
             src="/rArea.png"
             alt="Target Detection"
             width={500}
             height={400}
-            className="rounded-lg shadow-lg"
+            className="rounded-lg shadow-lg mr-30"
           />
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
-            Detect Target
-          </button>
       </section>
 
       {/* Crime Prevention Section */}
       <section
         id="crime"
-        className="flex items-center justify-center h-screen px-10 bg-red-100"
+        className="flex items-center justify-center h-screen px-10 bg-gray-100"
       >
-        <div className="text-left text-gray-900">
-          <h2 className="text-4xl font-bold mb-4">Crime Prevention</h2>
-          <p className="text-lg mb-6">
-            Real-time analysis of surveillance footage to prevent criminal
-            activities before they occur.
-          </p>
-          <Image
+        <Image
             src="/crime.jpeg"
             alt="Crime Prevention"
             width={500}
             height={400}
             className="rounded-lg shadow-lg"
           />
-        </div>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
+        <div className="text-left text-gray-900 ml-20">
+          <h2 className="text-4xl font-bold mb-4">Crime Prevention</h2>
+          <p className="text-lg mb-6">
+            Real-time checking of the crimes and send alerts to the <br /> concerned authorities regarding it.
+            Also locate <br /> the position of it in the map.
+          </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg">
             Prevent Crime
           </button>
+        </div>
+       
       </section>
     </div>
   );
